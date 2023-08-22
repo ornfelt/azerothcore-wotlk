3c9f0adf46e29ee632bf86ac4756ecd7c37b27d4
ornfelt <jonasornfelt@outlook.com>
2023-08-22 10:13:17 +0200

    Added Trickerer's npcbots (2023-08-22)

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -34,6 +34,12 @@
 #include "WorldPacket.h"
 #include "WorldSession.h"
 
+//npcbot: try query bot name
+#include "CreatureData.h"
+#include "botdatamgr.h"
+#include "botmgr.h"
+//end npcbot
+
 class Aura;
 
 /* differeces from off:
@@ -678,6 +684,17 @@ void WorldSession::HandleGroupChangeSubGroupOpcode(WorldPacket& recvData)
         guid = sCharacterCache->GetCharacterGuidByName(name);
     }
 
+    //npcbot
+    if (guid.IsEmpty())
+    {
+        if (Creature const* bot = BotDataMgr::FindBot(name, GetSessionDbcLocale()))
+            guid = bot->GetGUID();
+    }
+
+    if (guid.IsEmpty())
+        return;
+    //end npcbot
+
     group->ChangeMembersGroup(guid, groupNr);
 }
 
@@ -986,6 +1003,21 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode(WorldPacket& recvData)
     LOG_DEBUG("network", "WORLD: Received CMSG_REQUEST_PARTY_MEMBER_STATS");
     ObjectGuid Guid;
     recvData >> Guid;
+
+    //npcbot: try send bot group member info
+    if (Guid.IsCreature())
+    {
+        uint32 creatureId = Guid.GetEntry();
+        CreatureTemplate const* creatureTemplate = sObjectMgr->GetCreatureTemplate(creatureId);
+        if (creatureTemplate && creatureTemplate->IsNPCBot())
+        {
+            WorldPacket bpdata(SMSG_PARTY_MEMBER_STATS_FULL, 4+2+2+2+1+2*6+8+1+8);
+            BotMgr::BuildBotPartyMemberStatsPacket(Guid, &bpdata);
+            SendPacket(&bpdata);
+            return;
+        }
+    }
+    //end npcbot
 
     Player* player = HashMapHolder<Player>::Find(Guid);
     if (!player)
